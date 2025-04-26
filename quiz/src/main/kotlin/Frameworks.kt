bfdc1f62a36fb71c610cb13fa12f7b743a606b83
restuu <restu_u@outlook.com>
2025-04-26 17:06:32 +0700

    init

---
+++ quiz/src/main/kotlin/Frameworks.kt
@@ -0,0 +1,30 @@
+package com.restuu
+
+import io.ktor.http.*
+import io.ktor.resources.*
+import io.ktor.serialization.kotlinx.json.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.requestvalidation.RequestValidation
+import io.ktor.server.plugins.requestvalidation.ValidationResult
+import io.ktor.server.plugins.statuspages.*
+import io.ktor.server.resources.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+import kotlinx.serialization.Serializable
+import org.koin.dsl.module
+import org.koin.ktor.plugin.Koin
+import org.koin.logger.slf4jLogger
+
+fun Application.configureFrameworks() {
+    install(Koin) {
+        slf4jLogger()
+        modules(module {
+            single<HelloService> {
+                HelloService {
+                    println(environment.log.info("Hello, World!"))
+                }
+            }
+        })
+    }
+}
