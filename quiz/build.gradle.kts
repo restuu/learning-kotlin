8a2a7ed641a9087fd82ae06afcf614cd8125f099
restuu <restu_u@outlook.com>
2025-05-03 16:41:04 +0700

    feat: manage dependencies
    - add mongodb
    - upgrade logback to cater CVE

--- quiz/build.gradle.kts
+++ quiz/build.gradle.kts
@@ -24,14 +24,21 @@ dependencies {
     implementation(libs.ktor.server.resources)
     implementation(libs.ktor.server.host.common)
     implementation(libs.ktor.server.status.pages)
-    implementation(libs.koin.ktor)
-    implementation(libs.koin.logger.slf4j)
     implementation(libs.ktor.server.request.validation)
     implementation(libs.ktor.server.content.negotiation)
-    implementation(libs.ktor.serialization.kotlinx.json)
     implementation(libs.ktor.server.netty)
-    implementation(libs.logback.classic)
     implementation(libs.ktor.server.config.yaml)
+
+    implementation(libs.ktor.serialization.kotlinx.json)
+
+    implementation(libs.koin.ktor)
+    implementation(libs.koin.logger.slf4j)
+
+    implementation(libs.logback.classic)
+
+    implementation(libs.mongodb.driver.kotlin)
+    implementation(libs.mongodb.bson.kotlin)
+
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
 }
--- quiz/src/main/kotlin/Application.kt
+++ quiz/src/main/kotlin/Application.kt
@@ -1,11 +1,15 @@
 package com.restuu
 
+import com.restuu.plugins.configureFrameworks
+import com.restuu.plugins.configureRouting
+import com.restuu.plugins.configureSerialization
 import io.ktor.server.application.*
 
 fun main(args: Array<String>) {
     io.ktor.server.netty.EngineMain.main(args)
 }
 
+@Suppress("unused")
 fun Application.module() {
     configureFrameworks()
     configureSerialization()
