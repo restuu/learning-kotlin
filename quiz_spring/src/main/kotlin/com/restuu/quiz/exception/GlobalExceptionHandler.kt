caec11090b01253cde1546ba6f407f7e7dddd0db
restuu <restu_u@outlook.com>
2025-06-07 16:54:37 +0700

    make global exception handler

--- quiz_spring/src/main/kotlin/com/restuu/quiz/exception/GlobalExceptionHandler.kt
+++ quiz_spring/src/main/kotlin/com/restuu/quiz/exception/GlobalExceptionHandler.kt
@@ -3,8 +3,11 @@ package com.restuu.quiz.exception
 import com.restuu.quiz.shared.dto.response.ApiResponse
 import com.restuu.quiz.shared.dto.response.HTTPResponse
 import com.restuu.quiz.shared.dto.response.toResponseEntity
+import org.springframework.core.codec.DecodingException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.bind.support.WebExchangeBindException
+import org.springframework.web.server.ServerWebInputException
 
 @RestControllerAdvice
 class GlobalExceptionHandler {
@@ -16,4 +19,32 @@ class GlobalExceptionHandler {
         return ApiResponse.internalServerError().toResponseEntity()
     }
 
+    @ExceptionHandler(WebExchangeBindException::class)
+    suspend fun handleWebExchangeBindException(e: WebExchangeBindException): HTTPResponse<Unit> {
+        val errors = mutableListOf<String>()
+
+        e.bindingResult.fieldErrors.forEach {
+            errors.add("${it.field}: ${it.defaultMessage}")
+        }
+
+        return ApiResponse.badRequest(errors).toResponseEntity()
+    }
+
+    @ExceptionHandler(ServerWebInputException::class)
+    suspend fun handleServerWebInputException(e: ServerWebInputException): HTTPResponse<Unit> {
+        val errors = mutableListOf<String>()
+
+        when (val cause = e.cause) {
+            is DecodingException -> {
+                cause.localizedMessage.let { errors.add(it) }
+            }
+
+            else -> {
+                errors.add(cause?.message ?: "invalid request payload")
+            }
+        }
+
+        return ApiResponse.badRequest(errors).toResponseEntity()
+    }
+    
 }
